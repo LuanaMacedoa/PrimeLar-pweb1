1487d5420a9da673fb6be711182db305219b4081
Luana Gabriella <luana.gabriella@academico.ifpb.edu.br>
2026-05-25 14:22:00 -0300

    Update Supabase client initialization with hardcoded values

--- src/service/database-service.ts
+++ src/service/database-service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { environment } from '../environments/environment.runtime';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DatabaseService {
+  private readonly apiUrl = 'https://gwwzahbscsijbehvxlgz.supabase.co';
+  private readonly apiKey = 'sb_publishable_mX-d5Z2qs5uApfiUBUxjGg_pLrJw11H';
+
   public supabase: SupabaseClient = createClient(
-    environment.supabaseUrl,
-    environment.supabaseKey
+    this.apiUrl,
+    this.apiKey
   );
 }
