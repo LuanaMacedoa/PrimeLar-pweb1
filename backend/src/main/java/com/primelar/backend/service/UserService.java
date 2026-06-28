a702210fffed7dee7156221eb21a674070b37460
Luis Eduardo <eduardogomessoares97@gmail.com>
2026-06-27 23:53:32 -0300

    feat(backend): atualiza o SecurityFilter, criar validação via JWT em TokenService e atualiza autorizações de requests em SecurityConfig

--- backend/src/main/java/com/primelar/backend/service/UserService.java
+++ backend/src/main/java/com/primelar/backend/service/UserService.java
@@ -1,5 +1,20 @@
 package com.primelar.backend.service;
 
+import org.springframework.stereotype.Service;
+
+import com.primelar.backend.repository.UserRepository;
+
+import lombok.Data;
+
+@Service
+@Data
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    
 }
