be46d6592085a9995f95130039372d624d2c2597
luana <luanagabriellapsm@gmail.com>
2026-06-30 00:11:18 -0300

    feat: implmentado controller de resetar senha

--- backend/src/main/java/com/primelar/backend/controller/PasswordResetController.java
+++ backend/src/main/java/com/primelar/backend/controller/PasswordResetController.java
@@ -7,6 +7,7 @@ import com.primelar.backend.model.dto.request.PasswordResetRequest;
 import com.primelar.backend.model.dto.request.ResetPasswordRequest;
 import com.primelar.backend.model.dto.response.PasswordResetResponse;
 import com.primelar.backend.model.dto.response.ResetPasswordResponse;
+import com.primelar.backend.service.PasswordResetService;
 
 import jakarta.validation.Valid;
 
@@ -14,22 +15,32 @@ import jakarta.validation.Valid;
 @RequestMapping("/password")
 public class PasswordResetController {
 
+    private final PasswordResetService passwordResetService;
+
+    public PasswordResetController(PasswordResetService passwordResetService) {
+        this.passwordResetService = passwordResetService;
+    }
+
     @PostMapping("/forgot")
     public ResponseEntity<PasswordResetResponse> forgotPassword(
             @Valid @RequestBody PasswordResetRequest request) {
 
-        // TODO: implementar
+        passwordResetService.solicitarReset(request.getEmail());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new PasswordResetResponse(
+                "Se o e-mail estiver cadastrado, você receberá um link em breve."
+        ));
     }
 
     @PostMapping("/reset")
     public ResponseEntity<ResetPasswordResponse> resetPassword(
             @Valid @RequestBody ResetPasswordRequest request) {
 
-        // TODO: implementar
+        passwordResetService.confirmarReset(request.getToken(), request.getNewPassword());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new ResetPasswordResponse(
+                "Senha redefinida com sucesso!"
+        ));
     }
 
 }
